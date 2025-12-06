e303692cda7b9e9fb294313e467f6fd304f67891
tchamidaryl <tchamidaryl@gmail.com>
2025-12-06 02:59:18 +0100

    Header completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "./components/search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <main className="">
       <div className="pattern" />
 
       <div className="wrapper">
         <header>
+          <img src="./hero.png" alt="Hero Banner" />
           <h1 className="">Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
         </header>
       </div>
 
-      <p>Search</p>
+      <Search searchTerm={ searchTerm } setSearchTerm={ setSearchTerm } />
+      <h1 className="text-white">{ searchTerm }</h1>
     </main>
   );
 };
